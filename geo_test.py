351f07a9c68de221163cb3e8c85d8042d41457ad
Robert Craig <robert.craig@geomojo.org>
2010-02-15 15:22:34 +0000

    removed appid you should using yours ;-)

--- geo_test.py
+++ geo_test.py
@@ -1,6 +1,6 @@
 import yahoogeoservices
 
-geo = yahoogeoservices.YahooGeoServices('4KA3IXLV34EaOPbc_OUMQXGmqbFgcxBEVoqo0oyq08ZaDJTaOsHPB67cDN8ffTOSkgaFzjXniA--')
+geo = yahoogeoservices.YahooGeoServices('YOUR_APP_ID')
 londons = geo.geocode('london, uk')
 for place in londons:
     print "%s: %d" % (place.name, place.woeid)
@@ -16,5 +16,5 @@ for place in reversed:
 text = "London is much better town than Paris but the best town in the World is New York"
 places = geo.get_places(text)
 for p in places:
-    place = yahoogeoservices.GeoPlanetPlace(p[0], '4KA3IXLV34EaOPbc_OUMQXGmqbFgcxBEVoqo0oyq08ZaDJTaOsHPB67cDN8ffTOSkgaFzjXniA--')
+    place = yahoogeoservices.GeoPlanetPlace(p[0], 'YOUR_APP_ID')
     print "%s: %d" % (place.name, place.woeid)
